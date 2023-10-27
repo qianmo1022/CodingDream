5688ece32f6f80e6220137ce9d13d6d11e6f9108
@wuyeqianli <1535673062@qq.com>
2023-10-27 23:46:43 +0800

    第五次提交

--- 算法/array/leet27.js
+++ 算法/array/leet27.js
@@ -1,8 +1,16 @@
 /**
- * @param {string} s
- * @param {string[]} words
- * @return {number[]}
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
  */
-var findSubstring = function(s, words) {
-
+var removeElement = function(nums, val) {
+    const n = nums.length
+    let left = 0
+    for (let right = 0; right < n; right++) {
+        if (nums[right] !== val) {
+            nums[left] = nums[right]
+            left++
+        }
+    }
+    return left
 };
