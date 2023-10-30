685439c28ee2294567b2a5b7a2b21bedd0116689
@wuyeqianli <1535673062@qq.com>
2023-10-30 22:37:28 +0800

    第八天提交

---
+++ 算法/array/leet66.js
@@ -0,0 +1,14 @@
+var plusOne = function(digits) {
+    var len = digits.length;
+    var i = len - 1;
+    while (i >= 0) {
+        if (digits[i] < 9) {
+            digits[i] += 1;
+            return digits;
+        }
+        digits[i] = 0;
+        i--;
+    }
+    digits.unshift(1);
+    return digits;
+};
