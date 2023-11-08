b09f06d824d9473f803edb9daa46e926f3cd4893
@wuyeqianli <1535673062@qq.com>
2023-11-09 00:22:50 +0800

    leet12

---
+++ 算法/哈希表/leet12.js
@@ -0,0 +1,12 @@
+var intToRoman = function(num) {
+    var roman = "";
+    var romanNums = ["M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"];
+    var nums = [1000,900,500,400,100,90,50,40,10,9,5,4,1];
+    for(var i = 0; i < nums.length; i++){
+        while(num >= nums[i]){
+            roman += romanNums[i];
+            num -= nums[i];
+        }
+    }
+    return roman;
+};
