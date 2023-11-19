4f5e610b0bd0c3d530e0d27b640258b229280423
@wuyeqianli <1535673062@qq.com>
2023-11-20 00:06:29 +0800

    leet67

--- 算法/string/leet67.js
+++ 算法/string/leet67.js
@@ -1,23 +1,25 @@
 var addBinary = function(a, b) {
-    let result = '';
-    let carry = 0;
+    let result = '';//字符串
+    let carry = 0;//进位
     let i = a.length - 1;
     let j = b.length - 1;
     while (i >= 0 || j >= 0) {
-        let sum = carry;
+        let sum = carry;//让sum等于进位，因为进位是上一次的结果
         if (i >= 0) {
-            sum += parseInt(a[i]);
+            sum += parseInt(a[i]);//parseInt() 函数可解析一个字符串，并返回一个整数。
             i--;
         }
         if (j >= 0) {
             sum += parseInt(b[j]);
             j--;
         }
-        result = (sum % 2) + result;
-        carry = Math.floor(sum / 2);
+        result = (sum % 2) + result;//当前位的结果
+        carry = Math.floor(sum / 2);//进位所加的数
     }
     if (carry > 0) {
         result = carry + result;
     }
     return result;
 };
+
+console.log(addBinary('11', '1'));
