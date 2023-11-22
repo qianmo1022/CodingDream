c47c714b10f101e53b63f90a60d8c9b53f5645db
@wuyeqianli <1535673062@qq.com>
2023-11-22 15:53:20 +0800

    leet21

---
+++ 算法/linkList/leet21.js
@@ -0,0 +1,21 @@
+function ListNode(val, next) {
+    this.val = (val===undefined ? 0 : val)
+    this.next = (next===undefined ? null : next)
+}
+
+var mergeTwoLists = function(list1, list2) {
+    const ret = new ListNode(0, null);
+    let cur = ret;
+    while (list1 && list2) {
+        if (list1.val <= list2.val){
+            cur.next = list1;
+            list1 = list1.next;
+        }else {
+            cur.next = list2;
+            list2 = list2.next;
+        }
+        cur = cur.next;
+    }
+    cur.next = list1 ? list1 : list2;
+    return ret.next;
+};
