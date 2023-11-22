9e236cd9a8e98dd84d87f87c25b60a92cbf92413
@wuyeqianli <1535673062@qq.com>
2023-11-22 15:01:15 +0800

    leet2

--- 算法/linkList/leet2.js
+++ 算法/linkList/leet2.js
@@ -1,6 +1,4 @@
-
-
-  function ListNode(val, next) {
+function ListNode(val, next) {
       this.val = (val===undefined ? 0 : val)
       this.next = (next===undefined ? null : next)
  }
@@ -12,7 +10,23 @@
  */
 
 var addTwoNumbers = function(l1, l2) {
-    var value =l1.val
-    console.log(value);
+    let ret = new ListNode(0);//创建一个新的链表，头节点为0
+    let cur = ret;//cur指向ret
+    let carry = 0;//记录进位
+    while (l1 || l2 || carry) {
+         let sum = carry;
+         if (l1) {
+              sum += l1.val;
+              l1 = l1.next;
+         }
+         if (l2) {
+              sum += l2.val;
+              l2 = l2.next;
+         }
+         cur.next = new ListNode(sum % 10);//使用取余运算符，创建一个新的链表，头节点为sum % 10，从而得到当前位的值
+         cur = cur.next;//cur指向cur.next，即指向新的链表，得到当前位的值
+         carry = sum >= 10 ? 1 : 0;
+    }
+    return ret.next;
 };
 addTwoNumbers([2,4,3],[5,6,4])
