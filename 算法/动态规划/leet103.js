d2c1a56345491389d74436849908edd217490795
@wuyeqianli <1535673062@qq.com>
2023-11-29 17:00:19 +0800

    leet70.js

--- 算法/动态规划/leet103.js
+++ 算法/动态规划/leet103.js
@@ -4,7 +4,7 @@ var coinChange = function(coins, amount) {
     let dp = new Array(amount + 1).fill(Infinity);//dp[i]表示凑成金额i所需的最少硬币个数
     dp[0] = 0;//金额为0时，所需硬币个数为0
     for (let i = 0; i < coins.length; i++) {//遍历硬币
-        for (let j = coins[i]; j < amount + 1; j++) {//遍历金额
+        for (let j = coins[i]; j < amount + 1; j++) {//遍历金额，小于当前钱币的金额不用考虑
             dp[j] = Math.min(dp[j], dp[j - coins[i]] + 1);//状态转移方程,dp[j]表示凑成金额j所需的最少硬币个数
         }
     }
