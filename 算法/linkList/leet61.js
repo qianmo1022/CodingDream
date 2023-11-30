3f8d3e3e353eafcc0b3be6f8a5ec2af679efe284
@wuyeqianli <1535673062@qq.com>
2023-11-30 22:25:56 +0800

    leet61

--- 算法/linkList/leet61.js
+++ 算法/linkList/leet61.js
@@ -1,3 +1,19 @@
 var rotateRight = function(head, k) {
-    
+    const dummy = new ListNode(0,head);
+    let fast = dummy;
+    let slow = dummy;
+    let length = 0;
+    while (fast.next) {
+        length++;
+        fast = fast.next;
+    }
+    let count = length - k % length;
+    while (count > 0) {
+        slow = slow.next;
+        count--;
+    }
+    fast.next = dummy.next;
+    dummy.next = slow.next;
+    slow.next = null;
+    return dummy.next;
 };
