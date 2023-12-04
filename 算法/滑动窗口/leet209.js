a2d138e9f5d0143acdb5dc4a414763f09ad51dcb
@wuyeqianli <1535673062@qq.com>
2023-12-04 23:36:19 +0800

    leet209

--- 算法/滑动窗口/leet209.js
+++ 算法/滑动窗口/leet209.js
@@ -1,3 +1,20 @@
 var minSubArrayLen = function(target, nums) {
+    const len = nums.length;
+    if (len === 0) return 0;
 
+    let slow = 0;
+    let fast = 0;
+    let total = 0;
+    let minLength = Infinity;
+
+    while (fast < len) {
+        total += nums[fast];
+        while (total >= target) {//缩小到不满足条件
+            minLength = Math.min(minLength, fast - slow + 1);//更新最小长度，注意这里是fast - slow + 1，因为fast是从0开始的，所以长度要加1
+            total -= nums[slow];
+            slow++;
+        }
+        fast++;
+    }
+    return minLength === Infinity ? 0 : minLength;//如果minLength没有被赋值，说明没有找到满足条件的子数组，返回0
 };
