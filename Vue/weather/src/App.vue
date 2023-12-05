ddca22625ece0f4fcf7fac64f8f65aeea2dc861e
@wuyeqianli <1535673062@qq.com>
2023-12-05 21:48:15 +0800

    demo: weather

---
+++ Vue/weather/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <!-- 所有配了路由的.vue文件都会在这里显示 -->
+    <router-view> </router-view>
+  </div>
+
+
+</template>
+
+<script>
+  export default {
+    data() {
+      return {}
+    },
+  }
+</script>
+
+<style lang="css">
+* {
+  margin: 0;
+  padding: 0;
+}
+
+
+</style>
