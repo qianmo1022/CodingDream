d4664f9befc90dd71d47c72c09653bbc0a26ac40
@wuyeqianli <1535673062@qq.com>
2023-12-06 16:47:00 +0800

    leet145

---
+++ 算法/树/leet145.js
@@ -0,0 +1,31 @@
+// var postorderTraversal = function(root) {
+//     let res = []
+
+//     postorder = (root) => {
+//         if (!root) return
+//         postorder(root.left)
+//         postorder(root.right)
+//         res.push(root.val)
+//     }
+//     postorder(root)
+//     return res
+// };
+
+var postorderTraversal = function(root) {
+    if(!root) return []
+    const res = []
+    const stack = []
+    stack.push(root)
+
+    while(stack.length){
+        const top = stack.pop()
+        res.unshift(top.val)
+        if(top.left) {
+            stack.push(top.left)
+        }
+        if(top.right) {
+            stack.push(top.right)
+        }
+    }
+    return res
+};
