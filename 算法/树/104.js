f8f5650622bee3e7f6e3cd65f6698c14ee556266
@wuyeqianli <1535673062@qq.com>
2023-12-08 00:15:19 +0800

    leet104

---
+++ 算法/树/104.js
@@ -0,0 +1,34 @@
+// var maxDepth = function(root) {
+//     if(!root) {
+//         return 0//没有节点，深度为0，这里是递归的出口
+//     }else{
+//         const left = maxDepth(root.left)
+//         const right = maxDepth(root.right)
+//         return Math.max(left,right)+1//+1是因为要算上root这一层
+//     }
+// };
+
+function maxDepth(root) {
+    if (!root) {
+      return 0;
+    }
+  
+    let queue = [root];
+    let depth = 0;
+  
+    while (queue.length) {
+      depth++;
+      for (let i = 0; i < queue.length; i++) {//这里是关键，每次循环都是把当前层的节点都出队，然后把下一层的节点都入队，所以每次循环都是一层
+        const node = queue.shift();
+        if (node.left) {
+          queue.push(node.left);
+        }
+        if (node.right) {
+          queue.push(node.right);
+        }
+      }
+    }
+  
+    return depth;
+  }
+  
