18c0b07136df952fad01244af74a1a6e229cbfa7
@wuyeqianli <1535673062@qq.com>
2023-12-08 16:17:30 +0800

    LCR021

---
+++ 算法/linkList/advance/LCR021.js
@@ -0,0 +1,40 @@
+// var removeNthFromEnd = function(head, n) {
+//     const dummy = new ListNode(0,head)
+//     let fast = dummy
+    
+//     let len = 0
+//     // len - n + 1 == 倒数第n个
+//     while(fast.next){//计算链表总长度
+//         len++
+//         fast = fast.next
+//     }
+//     let slow = dummy
+
+//     for(let i = 0;i < len - n;i++){//slow走到倒数第n个节点的前一个节点
+//         slow = slow.next
+//     }
+
+//     if (slow.next) {//删除倒数第n个节点
+//         slow.next = slow.next.next
+//     }
+    
+//     return dummy.next
+
+// };
+
+var removeNthFromEnd = function(head, n) {
+    let dummy = new ListNode(0, head);
+    let fast = dummy;//快慢指针，后续的操作只改变了fast和slow对dummy的引用，而dummy.next没有改变
+    let slow = dummy;
+
+    while (n--) {//fast先走n步
+        fast = fast.next;
+    }
+    // 由于fast先走了n步，当fast走到最后一个节点时，slow相当于从后往前数的第n个节点
+    while (fast.next) {//fast走到最后一个节点时，slow刚好走到倒数第n个节点的前一个节点
+        fast = fast.next;
+        slow = slow.next;
+    }
+    slow.next = slow.next.next;//删除倒数第n个节点
+    return dummy.next;
+};
