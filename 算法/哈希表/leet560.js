68498b79086573285ad9f25b67bc01c0ebb5efaa
@wuyeqianli <1535673062@qq.com>
2023-12-10 00:50:11 +0800

    leet560

--- 算法/哈希表/leet560.js
+++ 算法/哈希表/leet560.js
@@ -14,11 +14,11 @@
 
 var subarraySum = function(nums, k) {
     const mp = new Map();
-    mp.set(0, 1);
+    mp.set(0, 1);// 初始化前缀和为0的个数为1，这样可以统计到前缀和等于k的情况
     let count = 0, pre = 0;
-    for (const x of nums) {
-        pre += x;
-        if (mp.has(pre - k)) {
+    for (const x of nums) {// 遍历数组
+        pre += x;// 计算前缀和
+        if (mp.has(pre - k)) {// 如果前缀和中存在pre - k，那么就说明存在一段区间和为k
             count += mp.get(pre - k);
         }
         if (mp.has(pre)) {
