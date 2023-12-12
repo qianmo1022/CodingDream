10fdb6d3d25a8aede445025d0f044d8ec08a0ff4
@wuyeqianli <1535673062@qq.com>
2023-12-12 21:41:29 +0800

    vue-sell

--- Vue/vue-sell/src/components/header/Header.vue
+++ Vue/vue-sell/src/components/header/Header.vue
@@ -1,35 +1,69 @@
 <template>
-    <div class="header">
+    <div class="header" @click="showDetail">
       <div class="content-wrapper">
         <div class="avatar">
-          <img src="http://static.galileo.xiaojukeji.com/static/tms/seller_avatar_256px.jpg" alt="">
+          <img :src="seller.avatar" alt="">
         </div>
         <div class="content">
           <div class="title">
             <span class="brand"></span>
-            <span class="name">店铺名称</span>
+            <span class="name">{{ seller.name }}</span>
           </div>
-          <div class="description">蜂鸟专送/38分钟送达</div>
-          <div class="support">
+          <div class="description">{{ seller.description }}/{{ seller.deliveryTime }}分钟送达</div>
+          <div class="support" v-if="seller.supports">
             <!--  -->
-            <span class="text">在线支付满28减5</span>
+            <support-icon size="1" :type="seller.supports[0].type" />
+            <span class="text">{{ seller.supports[0].description }}</span>
           </div>
         </div>
-        <div class="support-count">
-            <span class="count">5个</span>
-            <i class="iconfont">></i>
+        <div class="support-count" v-if="seller.supports">
+          <span class="count">{{ seller.supports.length }}个</span>
+          <i class="iconfont icon-arrow-right" ></i>
         </div>
       </div>
   
       <div class="bulletin-wrapper">
-  
+        <span class="bulletin-title"></span>
+        <span class="bulletin-text">{{ seller.bulletin }}</span>
+        <i class="iconfont icon-arrow-right" ></i>
       </div>
+
+      <div class="bg" v-if="seller.avatar" :style="{backgroundImage:`url(${seller.avatar})`}"></div>
+
+      <header-detail v-show="detailShow" @hide="handle" />
+
     </div>
+
   </template>
   
   <script>
+  import SupportIcon from '@/components/support-icon/Support-icon.vue'
+  import HeaderDetail from '@/components/header-detail/Header-detail.vue'
     export default {
-      
+      components: {
+        SupportIcon,
+        HeaderDetail
+      },
+      props: {
+        seller:{
+          type: Object,
+          default:() => {}
+        }
+      },
+      data() {
+        return {
+          detailShow:false
+        }
+      },
+      methods: {
+        showDetail(){
+          this.detailShow = true;
+        },
+        handle(val){
+          // console.log('子组件发布了一个事件，父组件接收到了',val);
+          this.detailShow = val;
+        }
+      }
     }
   </script>
   
@@ -38,6 +72,7 @@
   @import '@/common/style/mixin.less';
   .header{
     background: @color-background-ss;
+    position: relative;
     .content-wrapper{
       display: flex;
       padding: 24px 12px 18px 24px;
@@ -80,6 +115,7 @@
           align-items: center;
           .text{
             font-size: @fontsize-small-s;
+            margin-left: 4px;
           }
         }
       }
@@ -97,8 +133,7 @@
         align-items: center;
         color: @color-white;
         .count{
-            font-size: @fontsize-small-s;
-            
+          font-size: @fontsize-small-s;
         }
         .iconfont{
           font-size: @fontsize-small-s;
@@ -106,6 +141,44 @@
         }
       }
     }
-  
+    .bulletin-wrapper{
+      display: flex;
+      height: 28px;
+      padding: 0 8px;
+      background-color: @color-background-sss;
+      align-items: center;
+      color: @color-white;
+      .bulletin-title{
+        // width: 22px;
+        flex: 0 0 22px;
+        height: 12px;
+        .bg-image('bulletin');
+        background-size: 100% 100%;
+      }
+    }
+    .bulletin-text{
+      flex: 1;
+      margin-left: 4px;
+      font-size: @fontsize-small-s;
+      //单行显示，超出省略号
+      white-space: nowrap;
+      overflow: hidden;
+      text-overflow: ellipsis;
+    }
+    .icon-arrow-right{
+      flex: 0 0 10px;
+      font-size: @fontsize-small-s;
+    }
+    .bg{
+      position: absolute;
+      left: 0;
+      right: 0;
+      top: 0;
+      bottom: 0;
+      // background-image: url(http://static.galileo.xiaojukeji.com/static/tms/seller_avatar_256px.jpg);
+      background-size: 100% 100%;
+      filter: blur(10px);
+      z-index: -1;
+    }
   }
   </style>
--- Vue/vue-sell/src/router/index.js
+++ Vue/vue-sell/src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 
 const routes = [
-
+  
 ]
 
 const router = createRouter({
