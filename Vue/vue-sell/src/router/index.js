15930bb51e876c75d19214ca1437cbac775f8421
@wuyeqianli <1535673062@qq.com>
2023-12-13 22:44:32 +0800

    vue-sell5

--- Vue/vue-sell/src/router/index.js
+++ Vue/vue-sell/src/router/index.js
@@ -3,6 +3,10 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
   {
+    path: '/',
+    redirect: '/goods'
+  },
+  {
     path: '/goods',
     name: 'goods',
     component: () => import('@/views/goods')
--- Vue/vue-sell/src/views/goods/index.vue
+++ Vue/vue-sell/src/views/goods/index.vue
@@ -17,8 +17,35 @@
                     </li>
                 </ul>
             </div>
-            <div class="foods-wrapper">
-                右侧菜品
+            <div class="foods-wrapper" ref="fooodsWrapper">
+                <ul>
+                    <!-- 一个菜系 -->
+                    <li class="food-list" v-for="(item,index) in goods" :key="index" ref="foodList">
+                        <h1 class="title">{{ item.name }}</h1>
+                        <!-- 一道又一道菜 -->
+                        <ul>
+                            <li class="food-item" v-for="(i,idx) in  item.foods" :key="idx">
+                                <div class="pic">
+                                    <img :src="i.image" alt="">
+                                </div>
+                                <div class="content">
+                                    <h2 class="name">{{ i.name }}</h2>
+                                    <p class="desc">{{ i.description }}</p>
+                                    <div class="extra">
+                                        <span class="count">月售{{ i.sellCount }}份</span>
+                                        <span>好评率{{ i.rating }}%</span>
+                                    </div>
+                                    <div class="price">
+                                        <span class="now">￥{{ i.price }}</span>
+                                        <span class="old" v-if="i.oldPrice" >￥{{ i.oldPrice }}</span>
+                                    </div>
+                                    <!-- 加号 -->
+                                </div>
+     
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
             </div>
         </div>
         <div class="cart-wrapper">购物车</div>
@@ -37,7 +64,24 @@ import BScroll from '@better-scroll/core'
         data(){
             return{
                 goods:[],
-                currentIndex:0
+                // currentIndex:0,
+                foodsScroll:{},//被BScroll修饰后的容器
+                scrollY:0,
+                foodList:[],// 所有的菜系的DOM结构
+                listHeight:[],// 所有的菜系的DOM的高度
+            }
+        },
+        computed: {
+            currentIndex() {//计算属性,根据右侧滚动的位置，返回不同的下标
+                // 随着右侧滚动this.scrollY返回不同的下标
+                for(let i = 0; i < this.listHeight.length; i++){
+                    const h1 = this.listHeight[i]
+                    const h2 = this.listHeight[i + 1]
+                    if (!h2 || (this.scrollY >= h1 && this.scrollY < h2)) {
+                        return i
+                    }
+                }
+                return 0
             }
         },
         created(){
@@ -48,6 +92,7 @@ import BScroll from '@better-scroll/core'
                 
                 this.$nextTick(()=>{// $nextTick里面的回调会在页面加载完毕后才执行
                     this.betterScroll()
+                    this._calculateHeight()
                 })
             })
         },
@@ -58,10 +103,36 @@ import BScroll from '@better-scroll/core'
                 new BScroll(this.$refs.menuWrapper,{
                     click:true
                 })
+                this.foodsScroll = new BScroll(this.$refs.fooodsWrapper,{
+                    click: true,
+                    scrollY: true,
+                    probeType: 3
+                })
+
+                this.foodList = this.$refs.foodList
+
+                // 给右侧容器监听上了滚动事件
+                this.foodsScroll.on('scroll',pos => {
+                    // console.log(Math.round(Math.abs(pos.y)));
+                    this.scrollY = Math.round(Math.abs(pos.y))
+                })
             },
             selectMenu(i) {
                 // console.log(i);
                 this.currentIndex = i
+                // 让右侧的内容滚动到对应的位置
+                // console.log(this.$refs.foodList);
+                this.foodsScroll.scrollToElement(this.$refs.foodList[i],300)
+            },
+            _calculateHeight() {
+                //  this.foodList
+                let height = 0
+                this.listHeight.push(height)
+                this.foodList.forEach( li => {
+                    height += li.clientHeight//获取每个菜系的高度
+                    this.listHeight.push(height)//把每个菜系的高度存起来
+                })
+                console.log(this.listHeight);
             }
         }
     }
@@ -82,7 +153,7 @@ import BScroll from '@better-scroll/core'
         .menu-wrapper{
             flex: 0 0 80px;
             background: @color-background-ssss;
-            overflow: scroll;
+            // overflow: scroll;
             .menu-item{
                 padding: 0 14px;
                 font-size: @fontsize-small ;
@@ -98,6 +169,58 @@ import BScroll from '@better-scroll/core'
         }
         .foods-wrapper{
             flex: 1;
+            .title{
+                height: 26px;
+                line-height: 26px;
+                font-size: @fontsize-small;
+                color: rgb(147, 153, 159);
+                background-color: @color-background-ssss;
+                padding-left: 14px;
+                border-left: 1px solid #d9dde1;
+
+            }
+            .food-item{
+                display: flex;
+                padding: 18px;
+                .pic{
+                    flex: 0 0 57px;
+                    margin-right: 10px;
+                    img{
+                        width: 100%;
+                    }
+                }
+                .content{
+                    flex: 1;
+                    .name{
+                        font-size: @fontsize-medium;
+                        color: rgb(7, 17, 27);
+                        margin: 2px 0 8px 0;
+                    }
+                    .desc,.extra{
+                        font-size: @fontsize-small-s;
+                        color: rgb(147, 153, 159);
+                        margin-bottom: 8px;
+                        line-height: 10px;
+                        .count{
+                            margin-right: 12px;
+                        }
+                    }
+                    .price{
+                        font-weight: 700;
+                        line-height: 10px;
+                        .now{
+                            font-size: @fontsize-medium;
+                            color: @color-red;
+                            margin-right: 8px;
+                        }
+                        .old{
+                            font-size: @fontsize-small-s;
+                            color: rgb(147, 153, 159);
+                            text-decoration: line-through;
+                        }
+                    }
+                }
+            }
         }
     }
     .cart-wrapper{
