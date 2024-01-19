07b97a34b273da0d2b25ae430a346289df34b92b
@wuyeqianli <1535673062@qq.com>
2024-01-19 22:09:12 +0800

    leet77-V2

--- 算法/回溯/leet77.js
+++ 算法/回溯/leet77.js
@@ -1,18 +1,36 @@
-let result = []
-let path = []
+// let result = []
+// let path = []
+// var combine = function(n, k) {
+//   result = []
+//   combineHelper(n, k, 1)
+//   return result
+// };
+// const combineHelper = (n, k, startIndex) => {
+//   if (path.length === k) {
+//     result.push([...path])
+//     return
+//   }
+//   for (let i = startIndex; i <= n - (k - path.length) + 1; ++i) {
+//     path.push(i)
+//     combineHelper(n, k, i + 1)
+//     path.pop()
+//   }
+// }
+
 var combine = function(n, k) {
-  result = []
-  combineHelper(n, k, 1)
-  return result
+  const res = [], path = [];
+  backtracking(n, k, 1);
+  return res;
+  function backtracking (n, k, i){
+      const len = path.length;
+      if(len === k) {
+          res.push(Array.from(path));
+          return;
+      }
+      for(let a = i; a <= n + len - k + 1; a++) {
+          path.push(a);
+          backtracking(n, k, a + 1);
+          path.pop();
+      }
+  }
 };
-const combineHelper = (n, k, startIndex) => {
-  if (path.length === k) {
-    result.push([...path])
-    return
-  }
-  for (let i = startIndex; i <= n - (k - path.length) + 1; ++i) {
-    path.push(i)
-    combineHelper(n, k, i + 1)
-    path.pop()
-  }
-}
