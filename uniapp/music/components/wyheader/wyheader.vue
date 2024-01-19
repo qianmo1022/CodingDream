a0f1ce0c10b534eae640160795984669d7b8e57f
@wuyeqianli <1535673062@qq.com>
2024-01-19 21:27:32 +0800

    music

---
+++ uniapp/music/components/wyheader/wyheader.vue
@@ -0,0 +1,46 @@
+<template>
+	<view class="header">
+		<uni-icons type="bars" size="24" @click="showMenu"/>
+		<view class="content">
+			<!-- 添加一个插槽 -->
+			<slot name="content"></slot>
+		</view>
+		<uni-icons :type="icon" size="24" />
+	</view>
+	<view class="box"></view>
+</template>
+
+<script setup>
+import { useStore } from 'vuex'
+	
+	const store = useStore()
+
+	defineProps({
+		icon:{
+			type:String,
+			default:'mic'
+		}
+	})
+	const showMenu = () => {
+		store.commit('changeIsShowMenu',true)
+	}
+</script>
+
+<style lang="scss">
+.header{
+	height: 100rpx;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	padding: 0 15rpx;
+	position: fixed;
+	top: 0;
+	left: 0;
+	width: 100%;
+	box-sizing: border-box;
+	z-index: 999;
+}
+.box{
+	height: 100rpx;
+}
+</style>
