ab18c3dd586ba91348709ce57ae6d2db882b9d9e
@wuyeqianli <1535673062@qq.com>
2024-01-24 22:37:19 +0800

    leet93.js

---
+++ 算法/回溯/leet93.js
@@ -0,0 +1,21 @@
+var restoreIpAddresses = function(s) {
+    const res = [], path = [];
+    backtracking(0, 0)
+    return res;
+    function backtracking(i) {
+        const len = path.length;
+        if(len > 4) return;
+        if(len === 4 && i === s.length) {
+            res.push(path.join("."));
+            return;
+        }
+        for(let j = i; j < s.length; j++) {
+            const str = s.slice(i, j + 1);
+            if(str.length > 3 || +str > 255) break;
+            if(str.length > 1 && str[0] === "0") break;
+            path.push(str);
+            backtracking(j + 1);
+            path.pop()
+        }
+    }
+};
