6e2adedd8ba831efed4d79bbdf52bace3a3c1726
@wuyeqianli <1535673062@qq.com>
2024-02-13 23:13:50 +0800

    leet55

---
+++ 算法/贪心/leet55.js
@@ -0,0 +1,11 @@
+var canJump = function(nums) {
+    if(nums.length === 1) return true
+    let cover = 0
+    for(let i = 0; i <= cover; i++) {
+        cover = Math.max(cover, i + nums[i])
+        if(cover >= nums.length - 1) {
+            return true
+        }
+    }
+    return false
+};
