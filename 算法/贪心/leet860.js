03edef945de3d1648ee464736eeaba86888c9f2f
@wuyeqianli <1535673062@qq.com>
2024-02-17 22:45:08 +0800

    leet860

---
+++ 算法/贪心/leet860.js
@@ -0,0 +1,28 @@
+var lemonadeChange = function(bills) {
+    let fiveCount = 0
+    let tenCount = 0
+
+    for(let i = 0; i < bills.length; i++) {
+        let bill = bills[i]
+        if(bill === 5) {
+            fiveCount += 1
+        } else if (bill === 10) {
+            if(fiveCount > 0) {
+                fiveCount -=1
+                tenCount += 1
+            } else {
+                return false
+            }
+        } else {
+            if(tenCount > 0 && fiveCount > 0) {
+                tenCount -= 1
+                fiveCount -= 1 
+            } else if(fiveCount >= 3) {
+                fiveCount -= 3
+            } else {
+                return false
+            }
+        } 
+    }
+    return true
+};
