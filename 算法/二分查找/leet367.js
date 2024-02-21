5cb66582f9b64ea5b494f882281271b2b7f95825
@wuyeqianli <1535673062@qq.com>
2024-02-21 21:58:44 +0800

    leet367&leet374

---
+++ 算法/二分查找/leet367.js
@@ -0,0 +1,20 @@
+// var isPerfectSquare = function(num) {
+//     const x = Math.floor(Math.sqrt(num));
+//     return x * x === num;
+// };
+
+var isPerfectSquare = function(num) {
+    let left = 0, right = num;
+    while (left <= right) {
+        const mid = Math.floor((right - left) / 2) + left;
+        const square = mid * mid;
+        if (square < num) {
+            left = mid + 1;
+        } else if (square > num) {
+            right = mid - 1;
+        } else {
+            return true;
+        }
+    }
+    return false;
+};
---
+++ 算法/二分查找/leet374.js
@@ -0,0 +1,13 @@
+var guessNumber = function(n) {
+    let left = 1, right = n;
+    while (left < right) { // 循环直至区间左右端点相同
+        const mid = Math.floor(left + (right - left) / 2); 
+        if (guess(mid) <= 0) {
+            right = mid; // 答案在区间 [left, mid] 中
+        } else {
+            left = mid + 1; // 答案在区间 [mid+1, right] 中
+        }
+    }
+    // 此时有 left == right，区间缩为一个点，即为答案
+    return left;
+};
