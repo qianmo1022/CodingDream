0138053cc05bb7ea55d90b676ce031fe75f55538
@wuyeqianli <1535673062@qq.com>
2024-03-04 15:58:09 +0800

    leet377

---
+++ 算法/动态规划/leet377.js
@@ -0,0 +1,13 @@
+var combinationSum4 = function(nums, target) {
+    let dp = Array(target + 1).fill(0)
+    dp[0] = 1
+
+    for(let i = 0;i <= target;i++){
+        for(let j = 0; j < nums.length; j++){
+            if(i >= nums[j]){
+                dp[i] += dp[i - nums[j]]
+            }
+        }
+    }
+    return dp[target]
+};
