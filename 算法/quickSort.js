38833afeea9a8e3a2593ef1f65bb8d2a3182706a
@wuyeqianli <1535673062@qq.com>
2024-03-06 11:46:05 +0800

    leet300

--- 算法/quickSort.js
+++ 算法/quickSort.js
@@ -2,7 +2,7 @@ function quickSort(arr) {
   if (arr.length <= 1) {
     return arr;
   }
-  const pivot = arr[0];
+  const pivot = arr[0];// 基准值
   const left = [];
   const right = [];
   for (let i = 1; i < arr.length; i++) {
