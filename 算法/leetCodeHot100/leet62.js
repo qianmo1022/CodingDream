06007e2dd7ca09886f7e3a32c5c139881dd76d26
@wuyeqianli <1535673062@qq.com>
2024-03-10 23:29:06 +0800

    leet62

---
+++ 算法/leetCodeHot100/leet62.js
@@ -0,0 +1,30 @@
+// var uniquePaths = function(m, n) {
+//     const dp = Array(m).fill().map(item => Array(n))
+
+//     for (let i = 0; i < m; ++i) {
+//         dp[i][0] = 1
+//     }
+
+//     for (let i = 0; i < n; ++i) {
+//         dp[0][i] = 1
+//     }
+
+//     for (let i = 1; i < m; ++i) {
+//         for (let j = 1; j < n; ++j) {
+//             dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+//         }
+//     }
+//     return dp[m - 1][n - 1]
+// };
+
+var uniquePaths = function(m, n) {
+    let dp = new Array(m).fill(1).map(() => new Array(n).fill(1));
+    // dp[i][j] 表示到达（i，j） 点的路径数
+    for (let i=1; i<m; i++) {
+        for (let j=1; j< n;j++) {
+            dp[i][j]=dp[i-1][j]+dp[i][j-1];
+        }
+    }
+    return dp[m-1][n-1];
+
+};
