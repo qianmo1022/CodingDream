2758a2fb879d6b8303b8169ba3447f861bd8bd06
@wuyeqianli <1535673062@qq.com>
2024-03-16 14:08:46 +0800

    JS

--- interview/Hand-shredded/call_bind_apply/bind.js
+++ interview/Hand-shredded/call_bind_apply/bind.js
@@ -1,15 +1,31 @@
-Function.prototype.myBind = function(context, ...args) {  
-    if(typeof this !== 'function'){
+// Function.prototype.myBind = function(context, ...args) {  
+//     if(typeof this !== 'function'){
+//         throw new TypeError('Error')
+//     }
+//     // 保存this
+//     const self = this;
+//     // 返回一个函数
+//     return function F() {
+//         // 因为返回了一个函数，我们可以 new F()，所以需要判断
+//         if(this instanceof F){
+//             return new self(...args, ...arguments);
+//         }
+//         return self.apply(context, args.concat(...arguments));
+//     }
+// }
+
+Function.prototype.myBind = function(context,...args) {
+    if (typeof this !== 'function') {
         throw new TypeError('Error')
     }
-    // 保存this
-    const self = this;
-    // 返回一个函数
-    return function F() {
-        // 因为返回了一个函数，我们可以 new F()，所以需要判断
-        if(this instanceof F){
-            return new self(...args, ...arguments);
-        }
-        return self.apply(context, args.concat(...arguments));
+
+    context = context ? Object(context) : window
+
+    const fn = this
+
+    return function (...innerArgs) {
+        ArrayArgs = args.concat(innerArgs)
+
+        return fn.apply(context,ArrayArgs)
     }
 }
